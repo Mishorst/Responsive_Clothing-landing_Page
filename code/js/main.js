e39971ee82ba54393f5f125288ce2c873323a338
Misho Rostomashvili <Rostomashvili05@gmail.com>
2023-02-03 17:29:25 +0400

    Add products section, try to fix slides bug

--- code/js/main.js
+++ code/js/main.js
@@ -10,6 +10,23 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader);
 
 /////////////////   SWIPER PRODUCTS   ///////////////////
+let swiperProducts = new Swiper(".products_container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidePerView: "auto",
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 72,
+    },
+  },
+});
 
 /////////////////   SCROLL SECTIONS ACTIVE LINK   ///////////////////
 
